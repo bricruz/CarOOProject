0b5cc7e6df14617f2bdf930679ec9e779630d9d7
Brian Cruz <cruz.b.james@gmail.com>
2021-10-10 17:40:31 -0500

    Car OO Proj

---
+++ app.js
@@ -0,0 +1,54 @@
+class Vehicle {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    honk() {
+        return "Beep";
+    }
+    toString() {
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}`;
+    }
+}
+
+class Car extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 4;
+    }
+
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+    revEngine() {
+        return "VROOM!!!";
+    }
+}
+
+class Garage {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.vehicles = new Array();
+    }
+    add(vehicle) {
+        if (vehicle.__proto__.constructor.__proto__ === Vehicle) {
+            if (this.vehicles.length < this.capacity) {
+                this.vehicles.push(vehicle)
+                return "Vehicle added!";
+            }
+            else {
+                return "Sorry we're full";
+            }
+        }
+        else {
+            return "Only vehicles are allowed in here!"
+
+        }
+    }
+
+}
